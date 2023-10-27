c0daeb022ed04766d2697481c246207347b6992d
pierrevelcina <alban.pierrevelcin@etu.umontpellier.fr>
2023-10-27 14:14:36 +0200

    Product : - retrait route et template createProduct - modification du template product.html.twig avec l'utilisation des variables - Modification route /product : met en place le form de creation + la liste des produits -Creation des produits avec enregistrement du chemin de l'image et enregistrement de l'image sur le site - Creation du ProductManager + interface pour mise en place d'un id unique a l'image - Modification de la page shop pour l'utilisation du template produit - Ajout de la liste de produit a la route /shop - image par defaut au cas ou aucune defin pour le produit

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'shop')]
-    public function shop(): Response
+    public function shop(ProductRepository $productRepository): Response
     {
-        return $this->render('shop/shop.html.twig');
+        $products = $productRepository->findAll();
+        return $this->render('shop/shop.html.twig', ["products" => $products]);
     }
 
     #[Route('/shoppingCart', name: 'shoppingCart')]
