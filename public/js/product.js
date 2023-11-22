1f6747d8a29b2473dd66f51c0e570b7d7a36db60
pierrevelcina <alban.pierrevelcin@etu.umontpellier.fr>
2023-11-22 13:54:10 +0100

    product.js

---
+++ public/js/product.js
@@ -0,0 +1,16 @@
+function supprimerFeedy(event) {
+	let button = event.target;
+    let product = button.closest("#product");
+    let xhr = new XMLHttpRequest();
+    let URL = Routing.generate('deleteProduct', {"id": button.dataset.productId});
+    xhr.open("DELETE", URL, true);
+    xhr.onload = function () {
+        product.remove();
+    };
+    xhr.send(null);
+}
+
+let buttons = document.getElementsByClassName("delete-product");
+Array.from(buttons).forEach(function (button) {
+    button.addEventListener("click", supprimerFeedy);
+});
